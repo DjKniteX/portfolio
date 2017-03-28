b64ddfb47457699ec628a8eb8fe2e60e4cbd04ee
Nikki X <djknitex@outlook.com>
2017-03-28 16:16:55 -0500

    Work in Progress 1

---
+++ app.js
@@ -0,0 +1,6 @@
+$(function() {$('body').on('click', 'a.scrollable', function(event) {
+  var $anchor = $(this);
+  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+  event.preventDefault();
+  });
+});
