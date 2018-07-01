e5ee494a9b060027fcf89d605abb880334087605
Nikki X <djknitex@outlook.com>
2018-06-30 20:50:00 -0700

    fixing minor css problems

---
+++ v6/main.js
@@ -0,0 +1,44 @@
+function relocate_home()
+{
+     location.href = "#me";
+} 
+
+function relocate_blog() {
+    location.href = "http://blog.nikkix.me"
+}
+
+function relocate_linkedin() {
+    location.href = "https://www.linkedin.com/in/nikki-xaysanasy-8b8565a1/"
+}
+
+function relocate_github() {
+    location.href = "https://github.com/djknitex"
+}
+
+function relocate_email() {
+    location.href = "mailto:nxaysanasy@gmail.com"
+}
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
